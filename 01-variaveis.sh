76a4c973700cea19f13886da98915ad936be0587
Marlon <marloncostaesousa@gmail.com>
2025-07-01 08:40:46 -0300

    variáveis

--- 01-variaveis.sh
+++ 01-variaveis.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Teste"
+NOME="Marlon Sousa"
+NOME2="Marlon 
+
+Sousa"
+echo "$NOME"
+echo "$NOME2"
+
+NUMERO_01=23
+NUMERO_02=34
+TOTAL=$(($NUMERO_01+$NUMERO_02))
+echo "$TOTAL"
+
+#atribuindo comandos à minha variável
+SAIDA_CAT="$(cat /etc/passwd | grep timezone)"
+echo "$SAIDA_CAT"
